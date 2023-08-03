b613d94f034c54919bb0f68d40f2a6b7e82c696f
c5rogers <natenaelhaylemariam@gmail.com>
2023-08-03 12:22:44 -0400

    some other modifications

--- server/src/controller/services/SelledOrder.js
+++ server/src/controller/services/SelledOrder.js
@@ -1,10 +1,10 @@
 const SelledOrder = require('../../database/schemas/SelledOrder')
 const { isValidObjectId } = require('mongoose')
 
-module.exports.Selled_orders_get = (req, res) => {
+module.exports.Selled_orders_get = async(req, res) => {
     try {
-        const selled_orders = SelledOrder.find({}).populate("user_id", "_id username gender location profile").populate("product_id", "_id name image price rating catagory")
-        const selled_order_counts = SelledOrder.find({}).count()
+        const selled_orders = await SelledOrder.find({}).populate("user_id", "_id username gender location profile").populate("product_id", "_id name image price rating catagory")
+        const selled_order_counts = await SelledOrder.find({}).count()
         return res.status(200).json({ selled_orders, selled_order_counts })
     } catch (error) {
         console.log(error)
@@ -12,11 +12,11 @@ module.exports.Selled_orders_get = (req, res) => {
     }
 }
 
-module.exports.Selled_order_get = (req, res) => {
+module.exports.Selled_order_get = async(req, res) => {
     try {
         const selled_order_id = req.params.id
         if (isValidObjectId(selled_order_id)) {
-            const selled_order = SelledOrder.findOne({ _id: selled_order_id }).populate("user_id", "_id username gender location profile").populate("product_id", "_id name image price rating catagory")
+            const selled_order = await SelledOrder.findOne({ _id: selled_order_id }).populate("user_id", "_id username gender location profile").populate("product_id", "_id name image price rating catagory")
             return res.status(200).json(selled_order)
         } else {
             return res.status(400).json({ message: "Invalid selled order id" })
@@ -26,6 +26,17 @@ module.exports.Selled_order_get = (req, res) => {
         return res.status(500).json(error)
     }
 }
-module.exports.Selled_order_delete = (req, res) => {
-
+module.exports.Selled_order_delete = async(req, res) => {
+    try {
+        const selled_order_id = req.params.id
+        if (isValidObjectId(selled_order_id)) {
+            await SelledOrder.deleteOne({ _id: selled_order_id })
+            return res.status(200).json({ message: "Selled order deleted successfully" })
+        } else {
+            return res.status(400).json({ message: "Invalid selled order id" })
+        }
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json(error)
+    }
 }
