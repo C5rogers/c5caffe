a045de34c57b3ed5431dc0e1bbe69c2c275e0764
c5rogers <natenaelhaylemariam@gmail.com>
2023-08-03 12:31:58 -0400

    now all we needed datas are geting out only order is left to be done

--- server/src/controller/services/SelledOrder.js
+++ server/src/controller/services/SelledOrder.js
@@ -1,11 +1,24 @@
 const SelledOrder = require('../../database/schemas/SelledOrder')
 const { isValidObjectId } = require('mongoose')
+const { getIdFromToken } = require('../../utils/jwt')
 
 module.exports.Selled_orders_get = async(req, res) => {
     try {
         const selled_orders = await SelledOrder.find({}).populate("user_id", "_id username gender location profile").populate("product_id", "_id name image price rating catagory")
         const selled_order_counts = await SelledOrder.find({}).count()
         return res.status(200).json({ selled_orders, selled_order_counts })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json(error)
+    }
+}
+
+module.exports.Selled_order_mine_get = async(req, res) => {
+    try {
+        const token = req.cookies.jwt
+        const user_id = getIdFromToken(token)
+        const selled_order = await SelledOrder.find({ user_id }).populate("user_id", "_id username gender location profile").populate("product_id", "_id name image price rating catagory")
+        return res.status(200).json(selled_order)
     } catch (error) {
         console.log(error)
         return res.status(500).json(error)
--- server/src/routes/services/selledorder.js
+++ server/src/routes/services/selledorder.js
@@ -1,10 +1,12 @@
 const { Router } = require('express')
 const SelledOrderController = require('../../controller/services/SelledOrder')
+const middleware = require('../../middlewares/authCheck')
 
 const router = Router()
 
 router.get('/', SelledOrderController.Selled_orders_get)
-router.get('/:id', SelledOrderController.Selled_order_get)
-router.delete('/:id', SelledOrderController.Selled_order_delete)
+router.get('/mine', middleware.protect_with_auth, SelledOrderController.Selled_order_mine_get)
+router.get('/:id', middleware.protect_with_auth, SelledOrderController.Selled_order_get)
+router.delete('/:id', middleware.protect_with_auth, SelledOrderController.Selled_order_delete)
 
 module.exports = router
