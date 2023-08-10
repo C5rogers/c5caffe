f4a81f441fabe456ac5b73409de99e75bc285f25
c5rogers <natenaelhaylemariam@gmail.com>
2023-08-10 04:45:49 -0400

    some modifications

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,28 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }, 
-  css:['~/assets/css/main.css'],
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
-  app:{
-    head:{
-      title:'C5 Caffe',
-      meta:[
-        {
-          name:"description",
-          content:"This is the C5 Online caffe used to shop online based on real life scenario"
-        }
-      ],
-      link:[
-        {
-          rel:"stylesheet",
-          href:"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
-        }
-      ]
-    }
-  }
+  devtools: { enabled: true }
 })
