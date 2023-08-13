89ecc85be31c7d991663d73b9515864b9dce00ec
c5rogers <natenaelhaylemariam@gmail.com>
2023-08-13 00:22:22 -0400

    some modifications

--- client/components/Footer.vue
+++ client/components/Footer.vue
@@ -8,13 +8,13 @@ const handleGoHome=()=>{
 
 <template>
 
-    <footer class="w-full h-80 bg-primary text-white mt-auto relative shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)]">
+    <footer class=" w-full h-80 bg-primary text-white mt-auto relative shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)]">
         <!-- the right box -->
-        <div class="w-3/4 h-56 bg-white text-primary ml-auto rounded-tl-xl flex justify-between items-center px-3 gap-5 relative">
+        <div class="w-full sm:w-3/4 h-56 bg-white text-primary ml-auto sm:rounded-tl-xl flex justify-between items-center px-3 gap-5 relative">
             <!-- the left -->
-            <div class="w-1/4 flex items-center justify-center">
+            <div class="hidden md:flex w-1/4  items-center justify-center">
                 <!-- the absolute circle -->
-                <div class="flex w-56 h-56 items-center justify-center rounded-full absolute -bottom-20 -left-20 bg-white">
+                <div class="hidden md:flex w-56 h-56 items-center justify-center rounded-full absolute -bottom-20 -left-20 bg-white">
                     <!--the inner circle -->
                     <div class="w-3/4 h-3/4 flex items-center justify-center rounded-full bg-gray-200">
                         <!-- the logo -->
@@ -27,32 +27,32 @@ const handleGoHome=()=>{
                 </div>
             </div>
             <!-- the right content -->
-            <div class="w-3/4 h-full py-5 flex items-center justify-center gap-5 font-Roboto ">
+            <div class="w-full sm:w-3/4 h-full py-5 flex items-center justify-center gap-5 font-Roboto ">
 
                 <!-- the left content -->
-                <div class="h-full  w-1/2 flex flex-col gap-1 justify-center">
+                <div class="h-full w-1/2 flex flex-col gap-1 justify-center">
                     <!-- the main title -->
-                    <div class="text-5xl font-bold">
+                    <div class="text-xl md:text-3xl lg:text-clit-4xl font-bold">
                         C5 Online Caffe.
                     </div>
                     <!-- the location -->
-                    <div class="text-2xl font-light">
+                    <div class="text-lg md:text-xl lg:text-2xl font-light">
                         Ethiopia | Addis Abeba.
                     </div>
                     <!-- the social media links -->
                     <div class="w-full flex items-center justfy-center">
                         <ul class="w-full flex items-center gap-3">
                             <li>
-                                <a href="#" class="w-14 h-14 flex items-center bg-primary justify-center text-white rounded-full text-3xl"><i class="fa-brands fa-twitter"></i></a>
+                                <a href="#" class="w-7 h-7 sm:w-10 sm:h-10 md:w-14 md:h-14 flex items-center bg-primary justify-center text-white rounded-full text-lg sm:text-xl md:text-3xl"><i class="fa-brands fa-twitter"></i></a>
                             </li>
                             <li>
-                                <a href="#" class="w-14 h-14 flex items-center bg-primary justify-center text-white rounded-full text-3xl"><i class="fa-brands fa-instagram"></i></a>
+                                <a href="#" class="w-7 h-7 sm:w-10 sm:h-10 md:w-14 md:h-14 flex items-center bg-primary justify-center text-white rounded-full text-lg sm:text-xl md:text-3xl"><i class="fa-brands fa-instagram"></i></a>
                             </li>
                             <li>
-                                <a href="#" class="w-14 h-14 flex items-center bg-primary justify-center text-white rounded-full text-3xl"><i class="fa-brands fa-facebook"></i></a>
+                                <a href="#" class="w-7 h-7 sm:w-10 sm:h-10 md:w-14 md:h-14 flex items-center bg-primary justify-center text-white rounded-full text-lg sm:text-xl md:text-3xl"><i class="fa-brands fa-facebook"></i></a>
                             </li>
                             <li>
-                                <a href="#" class="w-14 h-14 flex items-center bg-primary justify-center text-white rounded-full text-3xl"><i class="fa-brands fa-tiktok"></i></a>
+                                <a href="#" class="w-7 h-7 sm:w-10 sm:h-10 md:w-14 md:h-14 flex items-center bg-primary justify-center text-white rounded-full text-lg sm:text-xl md:text-3xl"><i class="fa-brands fa-tiktok"></i></a>
                             </li>
                         </ul>
                     </div>
@@ -66,26 +66,26 @@ const handleGoHome=()=>{
                     <ul class="w-full h-full flex flex-col justify-center gap-1">
                         <li class="flex items-center gap-2">
                             <!-- the icon -->
-                            <span class="w-12 h-12 flex items-center justify-center rounded-full text-xl text-white bg-primary">
+                            <span class="w-7 h-7 sm:w-10 sm:h-10 md:w-12 md:h-12 flex items-center justify-center rounded-full text-sm sm:text-lg md:text-xl text-white bg-primary">
                                 <i class="fa-solid fa-phone"></i>
                             </span>
-                            <span class="font-light text-xl">+251939209166</span>
+                            <span class="font-light text-sm sm:text-lg md:text-xl">+251939209166</span>
                         </li>
                         <li class="flex items-center gap-2">
                             <!-- the icon -->
-                            <span class="w-12 h-12 flex items-center justify-center rounded-full text-xl text-white bg-primary">
+                            <span class="w-7 h-7 sm:w-10 sm:h-10 md:w-12 md:h-12 flex items-center justify-center rounded-full text-sm sm:text-lg md:text-xl text-white bg-primary">
                                 <i class="fa-solid fa-globe"></i>
                             </span>
-                            <span class="font-light text-xl">
+                            <span class="font-light text-sm sm:text-lg md:text-xl">
                                 www.natenael-haylemariam.com
                             </span>
                         </li>
                         <li class="flex items-center gap-2">
                             <!-- the icon -->
-                            <span class="w-12 h-12 flex items-center justify-center rounded-full text-xl text-white bg-primary">
+                            <span class="w-7 h-7 sm:w-10 sm:h-10 md:w-12 md:h-12 flex items-center justify-center rounded-full text-sm sm:text-lg md:text-xl text-white bg-primary">
                                 <i class="fa-solid fa-location-dot"></i>
                             </span>
-                            <span class="font-light text-xl">Ethiopia | Addis Abeba</span>
+                            <span class="font-light text-sm sm:text-lg md:text-xl">Ethiopia | Addis Abeba</span>
                         </li>
                     </ul>
                 </div>
