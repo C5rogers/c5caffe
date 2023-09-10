d2de0a033fb769d67d957e57ee41882c5c4f7eda
c5rogers <natenaelhaylemariam@gmail.com>
2023-09-10 05:59:57 -0400

    now circular users analysis also done

--- client/components/admin/CircularUsersSignupReport.vue
+++ client/components/admin/CircularUsersSignupReport.vue
@@ -1,18 +1,17 @@
 <script setup>
-
 import Chart from 'chart.js/auto'
 
 const useChart_dataStore=chart_dataStore()
 
 onMounted(()=>{
     if(useChart_dataStore.$state.users_signedup_perweek){
-        new Chart(document.getElementById('SignedUpUsersReport'),{
-            type:'radar',
+        new Chart(document.getElementById('CircularSignedupUserReport'),{
+            type:'doughnut',
             data:{
                 labels:useChart_dataStore.$state.users_signedup_perweek.labels,
                 datasets:[
                     {
-                        label:'Signed Up Users/Month',
+                        label:'Signed up users/Month',
                         data:useChart_dataStore.$state.users_signedup_perweek.data,
                         backgroundColor: ['#f57e16', '#f44336', '#9c27b0','#673ab7','#3f51b5','#2196f3','#03a9f4','#00bcd4','#009688','#4caf50','#8bc34a','#ffeb3b']
                     }
@@ -30,5 +29,5 @@ onMounted(()=>{
 </script>
 
 <template>
-    <canvas id="SignedUpUsersReport"></canvas>
+    <canvas id="CircularSignedupUserReport"></canvas>
 </template>
