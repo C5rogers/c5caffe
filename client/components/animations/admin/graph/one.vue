903bb5cf59135301b0d9c1fb4fe5fa36f3e5fb7b
c5rogers <natenaelhaylemariam@gmail.com>
2023-09-14 05:46:00 -0400

    now the graph placeholders are done

---
+++ client/components/animations/admin/graph/one.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="w-full h-full flex items-center justify-center overflow-hidden">
+        <img src="../../../../assets/images/graph placeholder1.gif" class="w-full h-full object-cover" />
+    </div>
+</template>
--- client/components/animations/admin/graph/two.vue
+++ client/components/animations/admin/graph/two.vue
@@ -1,5 +1,5 @@
 <template>
-    <!-- the holder -->
-    <div>
+    <div class="w-full h-full flex items-center justify-center overflow-hidden">
+        <img src="../../../../assets/images/graph placeholder 2.gif" class="w-full h-full object-cover" />
     </div>
 </template>
