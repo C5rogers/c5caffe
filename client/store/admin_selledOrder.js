d35e7e081b69b20f4b5efc1cbce4e93fed12342f
c5rogers <natenaelhaylemariam@gmail.com>
2023-09-20 07:06:29 -0400

    now the selled order store is done

--- client/store/admin_selledOrder.js
+++ client/store/admin_selledOrder.js
@@ -45,8 +45,79 @@ export const adminSelledOrderStore = defineStore({
             this.selled_orders_current_page = payload
         },
         async getSelledOrders(payload) {
-
+            try {
+                this.is_selled_orders_loading = true
+                const responce = await axiosInstance.get('/selled_order/?q=' + payload + '&page=' + this.selled_orders_current_page + '&limit=' + this.selled_orders_page_limit)
+                if (responce.status == 200 || responce.status == 201) {
+                    this.selled_orders = await responce.data.selled_orders
+                    this.selled_orders_count = await responce.data.selled_order_counts
+                    this.selled_orders_total_page = await responce.data.total_pages
+                    this.selled_orders_current_page = await responce.data.current_page
+                    this.is_selled_orders_loading = false
+                    return true
+                }
+            } catch (error) {
+                console.log(error)
+                this.is_selled_orders_loading = false
+                if (error.response) {
+                    this.errors = error.response.data
+                    if (error.response.data.message) {
+                        this.error_message = error.response.data.message
+                    }
+                }
+                if (error.code == 'ERR_NETWORK') {
+                    this.selled_orders_network_error = true
+                }
+                return false
+            }
         },
+        async getSelledOrder(payload) {
+            try {
+                this.is_selled_order_loading = true
+                const responce = await axiosInstance.get('/selled_order/' + payload)
+                if (responce.status == 200 || responce.status == 201) {
+                    this.selled_order = await responce.data.selled_order
+                    this.is_selled_order_loading = false
+                    return true
+                }
+            } catch (error) {
+                console.log(error)
+                this.is_selled_order_loading = false
+                if (error.response) {
+                    this.errors = error.response.data
+                    if (error.response.data.message) {
+                        this.error_message = error.response.data.message
+                    }
+                }
+                if (error.code == 'ERR_NETWORK') {
+                    this.selled_order_network_error = true
+                }
+                return false
+            }
+        },
+        async deleteSelledOrder(payload) {
+            try {
+                this.is_selled_order_loading = true
+                const responce = await axiosInstance.delete('/selled_order/' + payload)
+                if (responce.status == 200 || responce.status == 201) {
+                    this.success_message = await responce.data.message
+                    this.is_selled_order_loading = false
+                    return true
+                }
+            } catch (error) {
+                console.log(error)
+                this.is_selled_order_loading = false
+                if (error.response) {
+                    this.errors = error.response.data
+                    if (error.response.data.message) {
+                        this.error_message = error.response.data.message
+                    }
+                }
+                if (error.code == 'ERR_NETWORK') {
+                    this.selled_order_network_error = true
+                }
+            }
+        }
     },
     getters: {
 
